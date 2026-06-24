0debe753b5df89785686b86c2ee93ee03b26ded7
leozinlsb <leonardosilvab007@gmail.com>
2026-06-23 23:07:47 -0300

    fix: tamanho de fontes e tamanho dos cards dashboard atualizados

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,14 @@ import DashboardCard from "./components/DashboardCard/index.tsx"
 import styled from "styled-components";
 
 const AppContainer = styled.div`
-    background-color: #e9e9e9;
     height: 100vh;
-    width: 100vw;
-    font-family: "Inter", sans-serif;
-    font-optical-sizing: auto;
-    font-weight: 400;
-    font-style: normal;
 `
 
 const DashSection = styled.section`
     display: flex;
-    width: 100%;
-    
+    width: auto;
+    justify-content: center;
+    gap: 30px;
 `
 
 function App() {
--- src/components/DashboardCard/index.tsx
+++ src/components/DashboardCard/index.tsx
@@ -1,29 +1,23 @@
 import styled from "styled-components";
 
-const CardContainer = styled.div`
-    display: flex;
-    width: 100%;
-    justify-content: space-between;
-`
-
 const Card = styled.div`
     background-color: white;
-    padding: 20px;
+    padding: 30px;
+    flex: 1;
     border-radius: 10px;
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-    margin: 10px;
     display: flex;
     align-items: center;
 `
 
 const Title = styled.h2`
-    font-size: 24px;
+    font-size: clamp(16px, 1.5vw, 24px);
     font-weight: 600;
     color: #333;
 `
 
 const Value = styled.p`
-    font-size: 36px;
+    font-size: clamp(24px, 2.5vw, 36px);
     font-weight: bold;
     color: #000000;
 `
@@ -45,15 +39,13 @@ interface DashboardCardProps {
 
 function DashboardCard({ title, value, icon }: DashboardCardProps) {
     return (
-        <CardContainer>
-            <Card>
-                <Icon src={icon} alt="Icon" />
-                <div>
-                    <Title>{title}</Title>
-                    <Value>{value}</Value>
-                </div>
-            </Card>
-        </CardContainer>
+        <Card>
+            <Icon src={icon} alt="Icon" />
+            <div>
+                <Title>{title}</Title>
+                <Value>{value}</Value>
+            </div>
+        </Card>
     )
 }
 
